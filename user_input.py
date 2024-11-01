f1afac32062aed3d8e9e6190191e5507399cb048
Koderblack22 <ridwanmuda22@gmail.com>
2024-11-01 15:13:15 +0000

    python week one assignment completed

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# collect and store user name
+name = input("Please enter your name: ")
+print("Hello, " + name + "!")
+
+# collect and store user age
+age = input("please enter your age: ")
+print("you are " + age + " years old")
+
+#  collect and store location
+location = input("Please enter your location: ")
+print("and live in " + location )
+
+# personalized message 
+print("Hello, " + name + "! you are " + age + " years old and live in " + location)
